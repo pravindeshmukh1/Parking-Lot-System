f72358c782d5cdc82c41e4e00acf0db7531d01e4
pravindeshmukh1 <pravin1desh@gmail.com>
2020-04-27 17:59:41 +0530

    ADDED: Test case Module exist or not

--- test/parkingLotTest.js
+++ test/parkingLotTest.js
@@ -1,1 +1,8 @@
-console.log("Welcome to Parking Lot System");
+var expect = require(`chai`).expect;
+
+describe(`ParkingLotSystem`, () => {
+    it(`should exist`, () => {
+        var ParkingLotSystem = require(`../main/parkingLotSystem.js`);
+        expect(ParkingLotSystem).to.not.be.undefined;
+    })
+})
