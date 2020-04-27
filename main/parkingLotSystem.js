815d91cb2d5b6547f39345377e683ddf96b9a6f6
pravindeshmukh1 <pravin1desh@gmail.com>
2020-04-27 20:04:39 +0530

    ADDED: Module defined Parking Lot System

---
+++ main/parkingLotSystem.js
@@ -0,0 +1,3 @@
+class ParkingLotSystem {
+   
+}
--- test/parkingLotTest.js
+++ test/parkingLotTest.js
@@ -4,5 +4,5 @@ describe(`ParkingLotSystem`, () => {
     it(`should exist`, () => {
         var ParkingLotSystem = require(`../main/parkingLotSystem.js`);
         expect(ParkingLotSystem).to.not.be.undefined;
-    })
+    })                                                                                                                                                                                                                                  
 })
